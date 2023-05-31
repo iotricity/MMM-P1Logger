3645d2cb20c7390ec33d148f724a3a111d790396
iotricity <43491669+iotricity@users.noreply.github.com>
2023-05-31 19:13:57 +0200

    Add files via upload

--- MMM-P1Logger.js
+++ MMM-P1Logger.js
@@ -1,17 +1,17 @@
 /* Magic Mirror
  * Module: MMM-P1Logger
  *
- * By Jeroen van Vooren (GitHub: iotricity)
+ * By Jeroen van Vooren (Iotricity)
  * MIT Licensed.
  */
 
 Module.register("MMM-P1Logger", {
 	defaults: {
+		uuid: "ec8991ce-d736-4dea-b8d6-2eeea3aa07e6",
+		powerunits: "metric",
+		graphic: "color",
 		hours: 4,
-		uuid: '2b978e1c-0dfa-42bf-af8e-bfe256453228',
-		interval: 2,
-		powerunits: config.units,
-		graphic: "color"
+		interval: 3
 	},
 
 	start: function () {
@@ -24,6 +24,7 @@ Module.register("MMM-P1Logger", {
 		this.txtcontainer = null;
 		this.actpwrcontainer = 0;
 		this.tmpvar = 0;
+		this.xmldata = "";
 		
 		setInterval(() => {
 			self.doSomeShizzle();
@@ -38,44 +39,52 @@ Module.register("MMM-P1Logger", {
 
 	getDom: function () {
 		var mapP1Container = document.createElement('div');
+		this.p1container = mapP1Container;
 		mapP1Container.className = 'mapP1Container';
 
-		var mapP1ActPwr = document.createElement('div');
-		this.pwractcontainer = mapP1ActPwr;
-		mapP1ActPwr.className = 'mapP1ActPwr';
-		mapP1ActPwr.innerHTML = '';
-		mapP1Container.appendChild(mapP1ActPwr);
-
-		var mapP1PwrDayCons = document.createElement('div');
-		this.pwrdayconscontainer = mapP1PwrDayCons;
-		mapP1PwrDayCons.className = 'mapP1PwrDayCons';
-		mapP1PwrDayCons.innerHTML = '';
-		mapP1Container.appendChild(mapP1PwrDayCons);
-		
-		var mapP1PwrDayProd = document.createElement('div');
-		this.pwrdayprodcontainer = mapP1PwrDayProd;
-		mapP1PwrDayProd.className = 'mapP1PwrDayProd';
-		mapP1PwrDayProd.innerHTML = '';
-		mapP1Container.appendChild(mapP1PwrDayProd);
-		
-		var mapP1GasDayCons = document.createElement('div');
-		this.gasdayconscontainer = mapP1GasDayCons;
-		mapP1GasDayCons.className = 'mapP1GasDayCons';
-		mapP1GasDayCons.innerHTML = '';
-		mapP1Container.appendChild(mapP1GasDayCons);
-		
-		var mapP1WtrDayCons = document.createElement('div');
-		this.wtrdayconscontainer = mapP1WtrDayCons;
-		mapP1WtrDayCons.className = 'mapP1WtrDayCons';
-		mapP1WtrDayCons.innerHTML = '';
-		mapP1Container.appendChild(mapP1WtrDayCons);
-		
 		var mapP1Graph = document.createElement('div');
 		this.graphcontainer = mapP1Graph;
 		mapP1Graph.className = 'mapP1Graph';
 		mapP1Graph.innerHTML = 'Fetching P1 Data';
 		mapP1Container.appendChild(mapP1Graph);
 
+		var mapP1Stats = document.createElement('div');
+		this.statscontainer = mapP1Stats;
+		mapP1Stats.className = 'mapP1Stats';
+		mapP1Stats.innerHTML = '';
+		mapP1Container.appendChild(mapP1Stats);		
+
+		var mapP1ActPwr = document.createElement('div');
+		this.pwractcontainer = mapP1ActPwr;
+		mapP1ActPwr.className = 'mapP1ActPwr';
+		mapP1ActPwr.innerHTML = '';
+		mapP1Stats.appendChild(mapP1ActPwr);
+
+		// Get your icons from: https://fontawesome.com/v4/icons/
+		var mapP1PwrDayCons = document.createElement('div');
+		this.pwrdayconscontainer = mapP1PwrDayCons;
+		mapP1PwrDayCons.className = 'mapP1PwrDayCons';
+		mapP1PwrDayCons.innerHTML = '';
+		mapP1Stats.appendChild(mapP1PwrDayCons);
+		
+		var mapP1PwrDayProd = document.createElement('div');
+		this.pwrdayprodcontainer = mapP1PwrDayProd;
+		mapP1PwrDayProd.className = 'mapP1PwrDayProd';
+		mapP1PwrDayProd.innerHTML = '';
+		mapP1Stats.appendChild(mapP1PwrDayProd);
+		
+		var mapP1GasDayCons = document.createElement('div');
+		this.gasdayconscontainer = mapP1GasDayCons;
+		mapP1GasDayCons.className = 'mapP1GasDayCons';
+		mapP1GasDayCons.innerHTML = '';
+		mapP1Stats.appendChild(mapP1GasDayCons);
+		
+		var mapP1WtrDayCons = document.createElement('div');
+		this.wtrdayconscontainer = mapP1WtrDayCons;
+		mapP1WtrDayCons.className = 'mapP1WtrDayCons';
+		mapP1WtrDayCons.innerHTML = '';
+		mapP1Stats.appendChild(mapP1WtrDayCons);
+		
 		this.loaded = true;
 		
 		return mapP1Container;
@@ -93,20 +102,20 @@ Module.register("MMM-P1Logger", {
 			// Update actual usage graphic
 			if (this.cntr % (this.config.interval * 60) == 0 || this.firstgraph == false) {
 				// Update actual usage graphic after interval from config. On the first run, get all DOM components
-				this.graphcontainer.innerHTML = '<img src=https://offroaders.nl/P1View/image.php?action=actual&mode=' + this.config.graphic + '&t=' + this.config.hours + '&uuid=' + this.config.uuid + '&w=450&h=200&somerandomshit=' + Math.round(Math.random() * 10000) + '>';
+				this.graphcontainer.innerHTML = '<img width=450 height=200 src=https://offroaders.nl/P1View/image.php?action=actual&mode=' + this.config.graphic + '&t=' + this.config.hours + '&uuid=' + this.config.uuid + '&w=450&h=200&somerandomshit=' + Math.round(Math.random() * 10000) + '>';
 				this.firstgraph = true;
 			}
 			// Update actual values (updated every 10 seconds from P1 Logger)
 			if (this.cntr % 10 == 1) {
 				var queryString = 'https://offroaders.nl/P1View/queryx.php?uuid=' + this.config.uuid + '&somerandomshit=' + Math.round(Math.random() * 10000);
-				fetch(queryString)
+				fetch(queryString)						// fetch has some random delay, see https://stackoverflow.com/questions/72972814/setting-a-delay-in-fetch-api-call
 				  .then(response => response.text())
 				  .then((data) => {
-					xmldata = data;
+					this.xmldata = data;
 				  })				
-				if (xmldata != null) {
+				if (this.xmldata != null) {
 					parser = new DOMParser();
-					xmlDoc = parser.parseFromString(xmldata,"text/xml");					
+					xmlDoc = parser.parseFromString(this.xmldata, "text/xml");					
 					pwractcons = Number(xmlDoc.getElementsByTagName('pwrActCons')[0].childNodes[0].nodeValue);
 					pwractprod = Number(xmlDoc.getElementsByTagName('pwrActProd')[0].childNodes[0].nodeValue);
 					pwrtarif = Number(xmlDoc.getElementsByTagName('pwrTarif')[0].childNodes[0].nodeValue);  // 1=Low, 2=High
@@ -122,14 +131,14 @@ Module.register("MMM-P1Logger", {
 					wtrdaycons = Number(xmlDoc.getElementsByTagName('wtrDayCons')[0].childNodes[0].nodeValue);
 					if (pwractcons == 0 && pwractprod > 0) {
 						pwractcons = pwractprod;
-						this.pwractcontainer.innerHTML = '<span style="color:#c0ffc0;">' + pwractcons.toLocaleString() + '<sup>W</sup></span>';
+						this.pwractcontainer.innerHTML = '<span style="color:#d0ffd0;">' + this.convertDecimalSeperator(pwractcons) + '<sup>W</sup></span>';
 					} else {
-						this.pwractcontainer.innerHTML = this.convertDecimalSeperator(pwractcons.toLocaleString()) + '<sup>W</sup>';
+						this.pwractcontainer.innerHTML = this.convertDecimalSeperator(pwractcons) + '<sup>W</sup>';
 					}
-					this.pwrdayconscontainer.innerHTML = '<span align="center"style="width:12px; display:inline-block;"><i class="fas fa-plug dimmed" style="margin-right: 0.5em;"></i></span> <span align="right" style="width:77px; display:inline-block;">'  + this.convertDecimalSeperator((parseFloat(pwrdaycons) / 1000.0).toFixed(3)).toLocaleString() + '<sup>kWh</sup></span>';
-					this.pwrdayprodcontainer.innerHTML = '<span align="center"style="width:12px; display:inline-block;"><i class="fas fa-sun dimmed" style="margin-right: 0.5em;"></i></span> <span align="right" style="width:77px; display:inline-block;">'  + this.convertDecimalSeperator((parseFloat(pwrdayprod) / 1000.0).toFixed(3)).toLocaleString() + '<sup>kWh</sup></span>';
-					this.gasdayconscontainer.innerHTML = '<span align="center"style="width:12px; display:inline-block;"><i class="fas fa-fire dimmed" style="margin-right: 0.5em;"></i></span> <span align="right" style="width:77px; display:inline-block;">'  + this.convertDecimalSeperator((parseFloat(gasdaycons) / 1000.0).toFixed(3)).toLocaleString() + '<sup>m3</sup></span>';
-					this.wtrdayconscontainer.innerHTML = '<span align="center"style="width:12px; display:inline-block;"><i class="fas fa-tint dimmed" style="margin-right: 0.5em;"></i></span> <span align="right" style="width:77px; display:inline-block;">'  + this.convertDecimalSeperator((parseFloat(wtrdaycons) / 1000.0).toFixed(3)).toLocaleString() + '<sup>m3</sup></span>';
+					this.pwrdayconscontainer.innerHTML = '<span align="center" style="width:12px; text-align: center; display:inline-block;"><i class="fas fa-plug dimmed" style="margin-right: 0.5em;"></i></span> <span align="right" style="width:80px; display:inline-block;">'  + this.convertDecimalSeperator((parseFloat(pwrdaycons) / 1000.0).toFixed(3)) + '<sup>kWh</sup></span>';
+					this.pwrdayprodcontainer.innerHTML = '<span align="center" style="width:12px; text-align: center; display:inline-block;"><i class="fas fa-sun dimmed" style="margin-right: 0.5em;"></i></span> <span align="right" style="width:80px; display:inline-block;">'  + this.convertDecimalSeperator((parseFloat(pwrdayprod) / 1000.0).toFixed(3)) + '<sup>kWh</sup></span>';
+					this.gasdayconscontainer.innerHTML = '<span align="center" style="width:12px; text-align: center; display:inline-block;"><i class="fas fa-fire dimmed" style="margin-right: 0.5em;"></i></span> <span align="right" style="width:80px; display:inline-block;">'  + this.convertDecimalSeperator((parseFloat(gasdaycons) / 1000.0).toFixed(3)) + '<sup>m3</sup></span>';
+					this.wtrdayconscontainer.innerHTML = '<span align="center" style="width:12px; text-align: center; display:inline-block;"><i class="fas fa-tint dimmed" style="margin-right: 0.5em;"></i></span> <span align="right" style="width:80px; display:inline-block;">'  + this.convertDecimalSeperator((parseFloat(wtrdaycons) / 1000.0).toFixed(3)) + '<sup>m3</sup></span>';
 				} else {
 					this.actpwrcontainer.innerHTML = 'XML Empty';
 				}
@@ -138,11 +147,12 @@ Module.register("MMM-P1Logger", {
 	},
 	
 	convertDecimalSeperator: function(res) {
+		oldres = res.toLocaleString();
 		if ((1.1).toLocaleString().substring(1, 2) === '.' && this.config.powerunits === 'metric') {
-			newres = ((res.replace(',', '__COMMA__')).replace('.', ',')).replace('__COMMA__', '.');
+			newres = ((oldres.replace(',', '__COMMA__')).replace('.', ',')).replace('__COMMA__', '.');
 			return newres;
 		} else {
-			return res;
+			return oldres;
 		}
 	}
 });
